d877a874d2c8b5b2ab370bb3c9a796f2738dbead
Fabricia Rodrigues <fabriciarodrigues45@gmail.com>
2022-07-14 20:20:43 -0300

    prova 3

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import TaskList from "./Components/TaskList";
+import Footer from "./Components/Footer";
+import GlobalState from "./Context/globalState";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <GlobalState>
+        <View style={styles.container}>
+          <TaskList />
+          <Footer />
+        </View>
+      </GlobalState>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
   },
 });
